caa8c5001d72b76c8294f43003985fa1f8c21c82
Bulduper <dawid199912@gmail.com>
2021-04-17 16:13:00 +0200

    Got rid of a very heavy MD_Parola library

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 #include <Arduino.h>
-#include <MD_Parola.h>
 #include <TimeLib.h>
 #include <RTClib.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <simple_matrix.h>
 #include <OneButton.h>
 
 //8x8 segments amount
@@ -13,16 +13,26 @@
 #define DATA_PIN  11
 #define CS_PIN    10
 
-#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+//#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
 //other pins
-#define BTN  3
-#define LIGHT_SENSOR     A0
-#define THERMO     4
+#define BTN             3
+#define LIGHT_SENSOR    A0
+#define THERMO          4
+
+// Time drifting compensation
+#define YR_ADDR         0
+#define MO_ADDR         1
+#define DA_ADDR         2
+#define WD_ADDR         3
+#define HR_ADDR         4
+#define MI_ADDR         5
+#define SC_ADDR         6
+#define SECS_A_DAYx100  1000
 
 //real time clock module object
 RTC_DS1307 Rtc;
 //display object
-MD_Parola ledMatrix = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+simpleMatrix ledMatrix = simpleMatrix(CS_PIN,true);
 //DS18xx temperature sensor via OneWire interface
 OneWire oneWire(THERMO);
 DallasTemperature sensors(&oneWire);
@@ -42,10 +52,12 @@ bool isLightSensorTriggered(int currentMeasurement);
 int brightnessMeasure();
 void offsetMinutes();
 void updateTimeToRtc();
+void checkTimeDrift();
+time_t getLastSetTime();
 time_t getTimeUnix();
+void buttonInterrupt();
 
 void setup() {
-
   //Input Pin setup
   pinMode(BTN, INPUT_PULLUP);
   
@@ -56,12 +68,13 @@ void setup() {
   ledMatrix.begin();
 
   ledMatrix.setIntensity(0);
+  updateTimeToRtc();
 
   //short click
   button.attachClick(offsetMinutes);
-  //long, 1s press
+  //long, 2s press
   button.attachLongPressStop(updateTimeToRtc);
-  button.setPressTicks(1000);
+  button.setPressTicks(2000);
 }
 
 void loop() {
@@ -74,9 +87,18 @@ void loop1Hz()
   static unsigned long lastT;
   if(millis()-lastT>1000)
   {
+    static unsigned int iterator;
     refreshDisplay();
     lastT = millis();
+
+    // Every hour
+    if(++iterator>3600)
+    {
+      checkTimeDrift();
+      iterator=0;
+    }
   }
+
 }
 
 void refreshDisplay()
@@ -86,7 +108,6 @@ void refreshDisplay()
   if(isLightSensorTriggered(brightnessMeasure()))
     showTemperature(getTemperature());
   else showTime(hour(),minute());
-  ledMatrix.displayAnimate();
 }
 
 time_t getTimeUnix()
@@ -99,7 +120,7 @@ void showTime(uint8_t h,uint8_t m)
   char hhmm_str[6];
   sprintf(hhmm_str,"%02d:%02d",h,m);
 
-  ledMatrix.displayText(hhmm_str,PA_CENTER,0,0,PA_PRINT,PA_NO_EFFECT);
+  ledMatrix.print(hhmm_str,2);
 }
 
 void showTemperature(float t)
@@ -108,7 +129,7 @@ void showTemperature(float t)
   dtostrf(t,3,1,temp);
   strcat(temp,"C");
 
-  ledMatrix.displayText(temp,PA_CENTER,0,0,PA_PRINT,PA_NO_EFFECT);
+  ledMatrix.print(temp,2);
 }
 
 float getTemperature()
@@ -149,8 +170,48 @@ void offsetMinutes()
   setSyncProvider(nullptr);
 }
 
+// Every RTC (especially with no temp. compensation) builds some time drift over time.
+// To compensate it, we check how much time elapsed from the last calibration
+// And we multiply this by an empirically obtained value (drift per day)
+void checkTimeDrift()
+{
+  time_t elapsedTime = now()-getLastSetTime();
+  int secsDrift = elapsedTime/86400 * SECS_A_DAYx100 / 100;
+
+  // If the drift is big enough, compensate it
+  if(secsDrift>=10)
+  {
+    adjustTime(-secsDrift);
+    updateTimeToRtc();
+  }
+}
+
+// Read the date of last time update/set/calibration of RTC from built-in memory
+time_t getLastSetTime()
+{
+  tmElements_t lastSet;
+  lastSet.Day = Rtc.readnvram(DA_ADDR);
+  lastSet.Month = Rtc.readnvram(MO_ADDR);
+  lastSet.Year = Rtc.readnvram(YR_ADDR);
+  //lastSet.Wday = Rtc.readnvram(WD_ADDR);
+  lastSet.Hour = Rtc.readnvram(HR_ADDR);
+  lastSet.Minute = Rtc.readnvram(MI_ADDR);
+  lastSet.Second = Rtc.readnvram(SC_ADDR);
+  return makeTime(lastSet);
+}
+
 void updateTimeToRtc()
 {
+  tmElements_t nowElements;
+  breakTime(now(),nowElements);
   Rtc.adjust(DateTime(now()));
+  Rtc.writenvram(YR_ADDR,nowElements.Year);
+  Rtc.writenvram(MO_ADDR,nowElements.Month);
+  Rtc.writenvram(DA_ADDR,nowElements.Day);
+  //Rtc.writenvram(WD_ADDR,nowElements.Wday);
+  Rtc.writenvram(HR_ADDR,nowElements.Hour);
+  Rtc.writenvram(MI_ADDR,nowElements.Minute);
+  Rtc.writenvram(SC_ADDR,nowElements.Second);
+
   setSyncProvider(getTimeUnix);
 }
